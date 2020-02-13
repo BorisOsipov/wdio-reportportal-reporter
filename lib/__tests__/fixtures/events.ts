d878aba1592cf2f79b4415904a9cbf66f926e51d
Joe Paolini <joe.paolini@brambles.com>
2020-02-13 16:43:21 -0500

    chore: added unit tests for onSuiteEnd

--- lib/__tests__/fixtures/events.ts
+++ lib/__tests__/fixtures/events.ts
@@ -1,2 +1,2 @@
 export const suiteStartEvent = () => ({cid: "0-0", title: "foo", runner: {"0-0": {}}});
-export const suiteEndEvent = {cid: "0-0", title: "foo"};
+export const suiteEndEvent = () => ({cid: "0-0", title: "foo"});
